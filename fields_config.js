a43c429743425ee748f6fd94480c99e3fd1298c2
NoamBarashBiram <noambr1234@gmail.com>
2020-09-23 13:36:08 +0300

    First

---
+++ fields_config.js
@@ -0,0 +1,9 @@
+var auto = "Autonomous",
+    telOp = "TelOp",
+    placeholder = "PLACEHOLDER_DO_NOT_TOUCH",
+    placeholderChildren = {placeholder: ""},
+    placeholderAll = {auto: placeholderChildren, telOp: placeholderChildren};
+
+class Field {
+
+}
